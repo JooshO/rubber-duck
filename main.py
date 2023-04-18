0d576bf62310fee3b6cfcb7047d2a9f0c981d6b0
JooshO <jtoverbe@mtu.edu>
2023-04-18 15:55:13 -0400

    Some functional changes, some dataset changes

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ LANG_KEY = "lang"
 
 import random
 import nltk
+from os.path import exists
 # nltk.chat.eliza.eliza_chat()
 
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
-import openai
+import pickle 
 
 from collections import Counter
 import csv
@@ -69,6 +70,12 @@ def train():
         tag_prob[tag] = tag_counts[tag] / total
         for item, count in counter.items():
             training_dict[tag][item] = (count + 1) / (tag_counts[tag] + 2)
+
+    with open('tagprob.pkl', 'wb') as f:
+        pickle.dump(tag_prob, f)
+
+    with open('training_dict.pkl', 'wb') as f:
+        pickle.dump(training_dict, f)
     
 
 def process_message(input):
@@ -76,11 +83,13 @@ def process_message(input):
 
     for tag, prob in tag_prob.items():
         for word in input:
+            if tag not in prod.keys():
+                prod[tag] = 1
+            
             if word in training_dict[tag].keys():
-                if tag not in prod.keys():
-                    prod[tag] = 1
-                
                 prod[tag] *= training_dict[tag][word]
+            else:
+                prod[tag] *= 0.001
 
         if tag in prod.keys():
             prod[tag] *= prob
@@ -91,7 +100,8 @@ def process_message(input):
         prinf("None found")
         return
     probability, selected = max(probabilities, key=lambda item:item[0])
-    print(f'{selected} is the tag for the message, probaility is {probability}')
+    # print(f'{selected} is the tag for the message, probaility is {probability}')
+    return selected
 
     
 
@@ -106,9 +116,21 @@ def main():
     lemmatizer = WordNetLemmatizer()
     # data_dict[NAME_KEY] = input("Hello! What is your name?\n")
     # data_dict[LANG_KEY] = input(f"Hello {data_dict[NAME_KEY]}, what language or framework are you working in today?\n")
-    train()
+    global tag_prob
+    global training_dict
 
-    print("Alright, I am ready to listen!")
+    reviewing = False
+    
+    if exists('tagprob.pkl') and exists('training_dict.pkl'):
+        with open('tagprob.pkl', 'rb') as f:
+            tag_prob = pickle.load(f)
+
+        with open('training_dict.pkl', 'rb') as f:
+            training_dict = pickle.load(f)
+    else:
+        train()
+
+    print("> Alright, I am ready to listen!")
     finished = False
     while not finished:
         user_in = input().lower()
@@ -122,21 +144,37 @@ def main():
         tagged_words = nltk.pos_tag(words)
         # lemmatized_words = [lemmatizer.lemmatize(word, pos=pos_map[part]) for (word, part) in tagged_words]
         lemmatized_words = [lemmatizer.lemmatize(word) for (word, part) in tagged_words] 
-        process_message(lemmatized_words)
+        
+        output = process_message(lemmatized_words)
 
+        if reviewing:
+            if output == "end_review":
+                reviewing = False
+                # todo: different tags for solved and not solved.
+                print("> I hope I was able to assist in solving your problem!")
+                print("> If not, you can try reaching out to your TA or professor, or try visiting the CCLC in Rekhi hall!")
+            else:
+                print("> Quack quack")
+            continue
 
-        # processing here!
         if "quit" in words or "exit" in lemmatized_words:
-            print(user_in)
             finished = True
             break
 
-        # print(words)
-        # print(lemmatized_words)
-        # print(user_in)
+        if output == "code_review":
+            print("> Feel free to start typing your code in! \n> Remember that copying and pasting will be less effective for you than retyping the code.")
+            print("> I'll stay out of the way as you type, until you let me know that you are done!")
+            reviewing = True
+
+        if output == "loop":
+            print("> It sounds like you are having an issue with a loop.")
+            print("> It's always a good idea to take a closer loop at your loop conditions and what can cause you to break out of a loop.")
+            print("> So try looking at breaks and continues, and check where you change your loop condition.")
+            print("> Let me know if you would like to go over code!")
 
 
-    print("Good bye!")
+
+    print("> Good bye!")
 
 if __name__ == "__main__":
     main()
